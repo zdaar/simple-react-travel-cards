91f37b9a373e613a3a76fefebcf75895f131cf0e
zdaar <zdaryl@gmail.com>
2022-06-15 20:40:47 +0200

    Navbar, Content and Card component basics

---
+++ src/components/Content.js
@@ -0,0 +1,18 @@
+import React from "react";
+import data from "../data.js"
+import Card from "./Card.js"
+
+const travelCards = data.map(travel => {
+    return (
+        <Card
+        key={travel.id}
+        data={travel}
+        />
+    )
+}
+
+  )
+
+export default function Content() {
+    return ({travelCards})
+    }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import earthLogo from "../images/earth.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+        <img src={earthLogo} alt="a logo of the earth" />
+        my travel journal
+    </nav>
+  )
+ }
